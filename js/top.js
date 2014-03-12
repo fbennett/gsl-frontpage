4ef5748fc4a82bbc22d2f632cd0da5b90dfe5be5
Frank Bennett <biercenator@gmail.com>
2014-03-12 09:48:12 +0900

    Start and end times for sessions

--- js/top.js
+++ js/top.js
@@ -1,6 +1,32 @@
-function buildTimes(){};
-
 var pageDate = null;
+
+function buildTimes (node) {
+    var timesHTML = '<option value="0">Time</option>\n';
+    for (var j=8,jlen=20;j<jlen;j+=1) {
+        for (var k=0,klen=60;k<klen;k+=15) {
+            var time = j + ':';
+            var minutes = '' + k;
+            while (minutes.length < 2) {
+                minutes = '0' + minutes;
+            }
+            time += minutes;
+            var styling = ''
+            if (!k) {
+                styling = 'style="font-weight:bold;" '
+            }
+            timesHTML += '<option ' + styling + 'value="' + time + '">' + time + '</option>\n'
+        }
+    }
+    if (node) {
+        node.innerHTML = timesHTML;
+    } else {
+        var nodes = document.getElementsByClassName('times');
+        for (var i=0,ilen=nodes.length;i<ilen;i+=1) {
+            var node = nodes[i];
+            node.innerHTML = timesHTML;
+        }
+    }
+};
 
 function initializePage () {
     pageDate = new Date().getTime();
@@ -9,6 +35,10 @@ function initializePage () {
     document.getElementById('uploader-attachment-filename').style.width = (uploaderNodeWidth + 'px');
     var hiddenIframe = document.getElementById('hidden-iframe-id');
     hiddenIframe.addEventListener('load',completedUpload);
+    var sessionHourStart = document.getElementById('session-hour-start');
+    buildTimes(sessionHourStart);
+    var sessionHourEnd = document.getElementById('session-hour-end');
+    buildTimes(sessionHourEnd);
     setKeyboardHandlers();
     setSearchableBlurHandlers();
     setSearchableFocusHandlers();
