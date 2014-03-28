433ce7ef05234e35be4e23ee270827d6be45c899
Frank Bennett <biercenator@gmail.com>
2014-03-28 21:51:23 +0900

    Get dynamic feed API working

--- api/feed/pending/matchExec.js
+++ api/feed/pending/matchExec.js
@@ -3,21 +3,26 @@
     cogClass.prototype.exec = function (params, request, response) {
         var oops = this.utils.apiError;
         var sys = this.sys;
-
-
-        console.log("Well, I'm here (a) ...");
+        var userID = params.id;
+        var userKey = params.key;
 
         var pages = sys.pages;
         pages.reset();
         
-        var sql = 'SELECT eventID FROM events WHERE (strftime("%s","now")-strftime("%s",touchDate,"unixepoch"))/60>60 AND NOT published=1  ORDER BY pageDate DESC;';
+        // XXXX ADD ME BACK: (strftime("%s","now")-strftime("%s",touchDate,"unixepoch"))/60>60 AND 
+        var sql = 'SELECT eventID FROM events WHERE NOT published=1 ORDER BY pageDate DESC;';
         sys.db.all(sql,function(err,rows){
             if (err) {return oops(response,err,'pending(1)')};
 
             sys.runEvents(rows,response,processData);
 
             function processData (data) {
-                var page = pages.composeFeed(data,true)
+
+                var page = pages.composeFeed(data,null)
+
+                page = page.replace(/@@USER_ID@@/g,userID)
+                    .replace(/@@USER_KEY@@/g,userKey);
+
                 response.writeHead(200, {'Content-Type': 'application/atom+xml'});
                 response.end(page)
             }
