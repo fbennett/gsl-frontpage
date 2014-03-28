ef7590673b4acd89cc24ee43551bcf1c294cf6f3
Frank Bennett <biercenator@gmail.com>
2014-03-28 21:52:12 +0900

    Return role of caller when populating page from server

--- api/admin/top/readevent/matchExec.js
+++ api/admin/top/readevent/matchExec.js
@@ -5,8 +5,15 @@
         var sys = this.sys;
         var eventID = params.eventid;
 
+        var role = null;
+        var userObj = sys.admin[params.userkey];
+        if (userObj) {
+            role = userObj.role;
+        }
+
         function respondToServer(response,obj) {
             response.writeHead(200, {'Content-Type': 'application/json'});
+            obj.role = role;
             response.end(JSON.stringify(obj));
         };
 
