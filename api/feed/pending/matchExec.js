83b653357d15d11a057327118e1183a1bf65dcc8
Frank Bennett <biercenator@gmail.com>
2014-04-01 15:05:59 +0900

    Set zero delay on dynamic feed in demo mode only

--- api/feed/pending/matchExec.js
+++ api/feed/pending/matchExec.js
@@ -9,8 +9,14 @@
         var pages = sys.pages;
         pages.reset();
         
+        var delay = 30;
+        console.log("DELAY "+delay);
+        if (sys.output_style === 'demo') {
+            delay = 0;
+        }
+
         var sql = 'SELECT eventID FROM events WHERE (strftime("%s","now")-strftime("%s",touchDate,"unixepoch"))/60>30 AND NOT published=1 and status=0 ORDER BY pageDate DESC;';
-        sys.db.all(sql,function(err,rows){
+        sys.db.all(sql,[delay],function(err,rows){
             if (err) {return oops(response,err,'pending(1)')};
 
             sys.runEvents(rows,response,processData);
